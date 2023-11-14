3e62b2c5179cb61c0df3575bfbda27d5e243e9e3
kientran1507 <133241619+kientran1507@users.noreply.github.com>
2023-11-14 14:24:13 +0700

    Re-organize my projects

---
+++ Lab2/AimsProject/src/hust/soict/dsai/aims/Aims.java
@@ -0,0 +1,40 @@
+//TranTrungKien-20210494
+
+package hust.soict.dsai.aims;
+import hust.soict.dsai.aims.cart.Cart;
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+
+public class Aims {
+
+	public static void main(String[] args) {
+		// Create a new cart
+		Cart anOrder = new Cart();
+		
+		// Create new dvd objects and add them to the cart
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", 
+				"Animation", "Roger Allers", 87, 19.95f);
+		anOrder.addDigitalVideoDisc(dvd1);
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", 
+				"Science Fiction", "George Lucas", 87, 24.95f);
+		anOrder.addDigitalVideoDisc(dvd2);
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", 
+				"Animation", 18.99f);
+		anOrder.addDigitalVideoDisc(dvd3);
+		
+		
+		// Create new dvd objects and remove some old one from the cart
+		
+		DigitalVideoDisc dvd4 = new DigitalVideoDisc("Harry Potter and the Deathly Hallows - Part 1", 
+				"Fantasy", "J. K. Rowling", 146, 30.95f);
+		anOrder.addDigitalVideoDisc(dvd4);
+		anOrder.removeDigitalVideoDisc(dvd3);
+		DigitalVideoDisc dvd5 = new DigitalVideoDisc("Harry Potter and the Deathly Hallows - Part 2", 
+				"Fantasy", "J. K. Rowling", 130, 28.95f);
+		anOrder.addDigitalVideoDisc(dvd5);
+		anOrder.removeDigitalVideoDisc(dvd1);
+		
+		// Print total cost of the items in the cart
+		anOrder.printOrder();
+	}
+
+}
---
+++ Lab2/AimsProject/src/hust/soict/dsai/test/store/StoreTest.java
@@ -0,0 +1,22 @@
+//TranTrungKien-20210494
+
+package hust.soict.dsai.test.store;
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+import hust.soict.dsai.aims.store.Store;
+
+public class StoreTest {
+    public static void main(String[] args) {
+        // Create a Store with a capacity of 5 DVDs
+        Store myStore = new Store(5);
+
+        // Add DVDs to the store
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("Inception", "Sci-Fi", "Christopher Nolan", 148, 19.99f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("The Shawshank Redemption", "Drama", "Frank Darabont", 142, 15.99f);
+
+        myStore.addDVD(dvd1);
+        myStore.addDVD(dvd2);
+
+        // Remove a DVD from the store
+        myStore.removeDVD(1);
+    }
+}
