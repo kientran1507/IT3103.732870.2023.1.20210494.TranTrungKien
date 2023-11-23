2dfac6cd78ccb62c55c7f808ea29cd9244fdce3e
kientran1507 <133241619+kientran1507@users.noreply.github.com>
2023-11-23 15:21:25 +0700

    Testing all the major class

--- Lab4/AimsProject/src/hust/soict/ite6/test/store/StoreTest.java
+++ Lab4/AimsProject/src/hust/soict/ite6/test/store/StoreTest.java
@@ -7,16 +7,16 @@ import hust.soict.ite6.aims.store.Store;
 public class StoreTest {
     public static void main(String[] args) {
         // Create a Store with a capacity of 5 DVDs
-        Store myStore = new Store(5);
+        Store myStore = new Store();
 
         // Add DVDs to the store
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("Inception", "Sci-Fi", "Christopher Nolan", 148, 19.99f);
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("The Shawshank Redemption", "Drama", "Frank Darabont", 142, 15.99f);
 
-        myStore.addDVD(dvd1);
-        myStore.addDVD(dvd2);
+        myStore.addMedia(dvd1);
+        myStore.addMedia(dvd2);
 
         // Remove a DVD from the store
-        myStore.removeDVD(1);
+        myStore.removeMedia(1);
     }
 }
