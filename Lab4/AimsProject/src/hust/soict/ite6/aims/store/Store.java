8f098390bafbb425d1341fac2451960ec750b460
kientran1507 <133241619+kientran1507@users.noreply.github.com>
2023-11-23 15:17:43 +0700

    Update the Store class to work with Media

--- Lab4/AimsProject/src/hust/soict/ite6/aims/store/Store.java
+++ Lab4/AimsProject/src/hust/soict/ite6/aims/store/Store.java
@@ -1,37 +1,30 @@
 //TranTrungKien-20210494
 
 package hust.soict.ite6.aims.store;
-import hust.soict.ite6.aims.media.DigitalVideoDisc;
+import java.util.ArrayList;
+
+import hust.soict.ite6.aims.media.Media;
 
 public class Store {
-    private DigitalVideoDisc itemsInStore[]; // Array of DVDs available in the store
-
-    public Store(int capacity) {
-        itemsInStore = new DigitalVideoDisc[capacity];
-    }
+    // ArrayList to store the DVDs available in the store
+    private ArrayList<Media> itemsInStore = new ArrayList<Media>();
 
     // Method to add a DVD to the store
-    public void addDVD(DigitalVideoDisc dvd) {
-        for (int i = 0; i < itemsInStore.length; i++) {
-            if (itemsInStore[i] == null) {
-                itemsInStore[i] = dvd;
-                System.out.println("DVD \"" + dvd.getTitle() + "\" added to the store.");
-                return;
-            }
-        }
-        System.out.println("The store is full. Cannot add another DVD.");
+    public void addMedia(Media media) {
+        itemsInStore.add(media);
+        System.out.println("Media \"" + media.getTitle() + "\" added to the store.");
     }
 
     // Method to remove a DVD from the store
-    public void removeDVD(int id) {
-        for (int i = 0; i < itemsInStore.length; i++) {
-            if (itemsInStore[i] != null && itemsInStore[i].getID() == id) {
-                System.out.println("DVD \"" + itemsInStore[i].getTitle() + "\" removed from the store.");
-                itemsInStore[i] = null;
+    public void removeMedia(int id) {
+        for (Media media : itemsInStore) {
+            if (media != null && media.getID() == id) {
+                itemsInStore.remove(media);
+                System.out.println("Media \"" + media.getTitle() + "\" removed from the store.");
                 return;
             }
         }
-        System.out.println("No DVD found with ID " + id + " in the store.");
+        System.out.println("No media found with ID " + id + " in the store.");
     }
 }
 
