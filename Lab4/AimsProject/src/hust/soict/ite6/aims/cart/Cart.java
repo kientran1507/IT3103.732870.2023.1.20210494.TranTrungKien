5ebf8fcf04160ca7fc5a59e0ca1d53e60ff26a88
kientran1507 <133241619+kientran1507@users.noreply.github.com>
2023-11-23 15:07:31 +0700

    Update the Cart class to work with Media

--- Lab4/AimsProject/src/hust/soict/ite6/aims/cart/Cart.java
+++ Lab4/AimsProject/src/hust/soict/ite6/aims/cart/Cart.java
@@ -1,74 +1,38 @@
 //TranTrungKien-20210494
 
 package hust.soict.ite6.aims.cart;
+import java.util.ArrayList;
+
 import hust.soict.ite6.aims.media.DigitalVideoDisc;
+import hust.soict.ite6.aims.media.Media;
 
 public class Cart {
     // Maximum number of DVDs that can be added to the cart
     public static final int MAX_NUMBERS_ORDERED = 20;
     
-    // Array to store DigitalVideoDisc objects
-    private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+    // ArrayList to store the ordered items
+    private ArrayList<Media> itemsOrdered = new ArrayList<Media>();
     
-    // Quantity of DigitalVideoDiscs currently in the cart
-    private int qtyOrdered;
-
     // Constructor to initialize the cart with no items
-    public Cart() {
-        qtyOrdered = 0;
-    }
+    public Cart() {}
 
-    // Method to add a DigitalVideoDisc to the cart
-    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
-        if (qtyOrdered <= MAX_NUMBERS_ORDERED) {
-            itemsOrdered[qtyOrdered] = disc;
-            qtyOrdered++;
-            System.out.println("The disc has been added!");
+    // Method to add media to the cart
+    public void addMedia(Media media) {
+        if (itemsOrdered.size() < MAX_NUMBERS_ORDERED) {
+            itemsOrdered.add(media);
+            System.out.println("Added to cart: " + media.getTitle());
         } else {
-            System.out.println("The cart is full. You cannot add another one!");
+            System.out.println("The cart is full. Cannot add more items.");
         }
     }
 
-    // Method to add a list of DigitalVideoDisc to the cart
-    public void addDigitalVideoDisc(DigitalVideoDisc[] dvdList) {
-        if (qtyOrdered + dvdList.length <= MAX_NUMBERS_ORDERED) {
-            for (DigitalVideoDisc disc : dvdList) {
-                itemsOrdered[qtyOrdered] = disc;
-                qtyOrdered++;
-            }
-            System.out.println("The discs have been added!");
+    // Method to remove media from the cart
+    public void removeMedia(Media media) {
+        if (itemsOrdered.remove(media)) {
+            System.out.println("Removed from cart: " + media.getTitle());
         } else {
-            System.out.println("The cart is full. You cannot add another one!");
+            System.out.println("Item not found in the cart: " + media.getTitle());
         }
-    }
-    
-    // Method to add 2 DigitalVideoDisc to the cart
-    public void addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
-    	if (qtyOrdered + 2 <= MAX_NUMBERS_ORDERED) {
-    		itemsOrdered[qtyOrdered] = dvd1;
-    		itemsOrdered[qtyOrdered+1] = dvd2;
-    		qtyOrdered+=2;
-    		System.out.println("Two discs have been added!");
-    	} else {
-    		System.out.println("The cart is full. You cannot add another one!");
-    	}
-    }
-
-    // Method to remove a DigitalVideoDisc from the cart
-    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
-        for (int i = 0; i < qtyOrdered; i++) {
-            if (itemsOrdered[i] == disc) {
-                // Shift elements to fill the gap created by removing the disc
-                for (int j = i; j < qtyOrdered - 1; j++) {
-                    itemsOrdered[j] = itemsOrdered[j + 1];
-                }
-                itemsOrdered[qtyOrdered - 1] = null; // Remove the last element
-                qtyOrdered--;
-                System.out.println("The disc has been removed.");
-                return;
-            }
-        }
-        System.out.println("The disc is not in the cart.");
     }
     
     // Method to print the list of ordered items, their prices, and total cost
@@ -76,9 +40,11 @@ public class Cart {
         System.out.println("***********************CART***********************");
         System.out.println("Ordered Items:");
 
-        for (int i = 0; i < qtyOrdered; i++) {
-            DigitalVideoDisc dvd = itemsOrdered[i];
-            System.out.println((i + 1) + ". DVD - " + dvd);
+        for (Media media : itemsOrdered) {
+            if (media instanceof DigitalVideoDisc) {
+                DigitalVideoDisc dvd = (DigitalVideoDisc) media;
+                System.out.println("DVD - " + dvd);
+            }
         }
 
         System.out.println("Total cost is: " + totalCost() + " $");
@@ -87,31 +53,37 @@ public class Cart {
 
     // Method to search for DVDs in the cart by ID and display the results
     public void searchByID(int id) {
-        for (DigitalVideoDisc dvd : itemsOrdered) {
-            if (dvd != null && dvd.getID() == id) {
-                System.out.println("Found DVD with ID " + id + ": " + dvd);
-                return;
-            }
-        }
+    	for (Media media : itemsOrdered) {
+    		if (media instanceof DigitalVideoDisc) {
+    			DigitalVideoDisc dvd = (DigitalVideoDisc) media;
+    			if (dvd.getID() == id) {
+    				System.out.println("Found DVD with ID " + id + ": " + dvd);
+    				return;
+    			}
+    		}
+    	}
         System.out.println("No DVD found with ID " + id + " in the cart.");
     }
 
     // Method to search for DVDs in the cart by title and display the results
     public void searchByTitle(String title) {
-        for (DigitalVideoDisc dvd : itemsOrdered) {
-            if (dvd != null && dvd.getTitle() == title) {
-                System.out.println("Found DVD with title \"" + title + "\": " + dvd);
-                return;
+    	for (Media media : itemsOrdered) {
+    		if (media instanceof DigitalVideoDisc) {
+    			DigitalVideoDisc dvd = (DigitalVideoDisc) media;
+    			if (dvd.getTitle().equals(title)) {
+    				System.out.println("Found DVD with title \"" + title + "\": " + dvd);
+    				return;
+                }
             }
-        }
+    	}
         System.out.println("No DVD found with title \"" + title + "\" in the cart.");
     }
 
     // Method to calculate the total cost of items in the cart
     public float totalCost() {
-        float totalCost = 0.0f;
-        for (int i = 0; i < qtyOrdered; i++) {
-            totalCost += itemsOrdered[i].getCost();
+    	float totalCost = 0.0f;
+    	for (Media media : itemsOrdered) {
+    		totalCost += media.getCost();
         }
         return totalCost;
     }
