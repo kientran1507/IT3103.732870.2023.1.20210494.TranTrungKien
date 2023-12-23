331354026e8f9f628d50e3d0699551dc3802b463
kientran1507 <133241619+kientran1507@users.noreply.github.com>
2023-12-23 18:38:46 +0700

    Create a View Store Screen

--- Lab4/AimsProject/src/hust/soict/ite6/aims/screen/StoreScreen.java
+++ Lab4/AimsProject/src/hust/soict/ite6/aims/screen/StoreScreen.java
@@ -1,113 +1,47 @@
+//TranTrungKien-20210494
+
 package hust.soict.ite6.aims.screen;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.util.ArrayList;
+import java.io.IOException;
 
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JPanel;
 
-import hust.soict.ite6.aims.media.Media;
 import hust.soict.ite6.aims.store.Store;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
 public class StoreScreen extends JFrame{
-	private Store store;
-	JPanel createNorth() {
-		JPanel north = new JPanel();
-		north.setLayout(new BoxLayout(north, BoxLayout.Y_AXIS));
-		north.add(createMenuBar());
-		north.add(createHeader());
-		return north;
-	}
+	private static Store store = new Store();
 	
-	JMenuBar createMenuBar() {
+	public StoreScreen (Store store) {
+		super();
 		
-		JMenu menu = new JMenu("Options");
-		
-		JMenu smUpdateStore = new JMenu("Update Store");
-		smUpdateStore.add(new JMenuItem("Add Book"));
-		smUpdateStore.add(new JMenuItem("Add CD"));
-		smUpdateStore.add(new JMenuItem("Add DVD"));
-		
-		menu.add(smUpdateStore);
-		menu.add(new JMenuItem("View store"));
-		menu.add(new JMenuItem("View cart"));
-		
-		JMenuBar menuBar = new JMenuBar();
-		menuBar.setLayout(new FlowLayout(FlowLayout.LEFT));
-		menuBar.add(menu);
-		
-		return menuBar;
-	}
-	
-	JPanel createHeader() {
-
-		JPanel header = new JPanel();
-		header.setLayout(new BoxLayout(header, BoxLayout.X_AXIS));
-		
-		JLabel title = new JLabel("AIMS");
-		title.setFont(new Font(title.getFont().getName(), Font.PLAIN, 50));
-		title.setForeground(Color.CYAN);
-		
-		JButton cart = new JButton("View cart");
-		cart.setPreferredSize(new Dimension(100, 50));
-		cart.setMaximumSize(new Dimension(100, 50));
-		
-		header.add(Box.createRigidArea(new Dimension(10, 10)));
-		header.add(title);
-		header.add(Box.createHorizontalGlue());
-		header.add(cart);
-		header.add(Box.createRigidArea(new Dimension(10, 10)));
-		
-		return header;
-	}
-	
-	JPanel createCenter() {
-		
-		JPanel center = new JPanel();
-		center.setLayout(new GridLayout(3, 3, 2, 2));
-		
-		ArrayList<Media> mediaInStore = store.getItemsInStore();
-		int loopSize = Math.min(9, mediaInStore.size());  // Limit loop size to 9 or actual list size
-		for(int i = 0; i < loopSize; i++) {
-		    MediaStore cell = new MediaStore(mediaInStore.get(i));
-		    center.add(cell);
-		}
-		
-		return center;
-	}
-	
-	public StoreScreen(Store store) {
 		this.store = store;
-		Container cp = getContentPane();
-		cp.setLayout(new BorderLayout());
 		
-		cp.add(createNorth(), BorderLayout.NORTH);
-		cp.add(createCenter(), BorderLayout.CENTER);
+		JFXPanel fxPanel = new JFXPanel();
+		this.add(fxPanel);
 		
-		setVisible(true);
-		setTitle("Store");
-		setSize(1025,768);
+		this.setTitle("Store");
+		this.setVisible(true);
+		Platform.runLater(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					FXMLLoader loader = new FXMLLoader(getClass().
+							getResource("/hust/soict/ite6/aims/screen/Store.fxml"));
+					StoreScreenController controller = new StoreScreenController(store);
+					loader.setController(controller);
+					Parent root = loader.load();
+
+					fxPanel.setScene(new Scene(root));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
-	
-    public static void main(String[] args) {
-        Store store = new Store();  // Create or obtain an instance of Store
-        new StoreScreen(store);
-    }
+
 }
-
-
-
-
